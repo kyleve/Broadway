c604406c2d5d7a190c157f4f9925414740bebd4a
Kyle Van Essen <k@kve.me>
2026-03-03 09:52:13 -0800

    Add initial version of CopyOnWrite struct. Update gitignore to include DS_Store.

---
+++ BroadwayCore/Sources/CopyOnWrite.swift
@@ -0,0 +1,56 @@
+//
+//  CopyOnWrite.swift
+//  BroadwayCore
+//
+//  Created by Kyle Van Essen on 3/3/26.
+//
+
+import Foundation
+
+
+@propertyWrapper public struct CopyOnWrite<Value> {
+    
+    public init(wrappedValue : Value) {
+        box = .init(value: wrappedValue)
+    }
+    
+    public var wrappedValue : Value {
+        get {
+            _unsafeUnderlyingValue
+        }
+        
+        set {
+            if !isKnownUniquelyReferenced(&box) {
+                box = .init(value: newValue)
+            } else {
+                box.value = newValue
+            }
+        }
+    }
+    
+    public var _unsafeUnderlyingValue : Value {
+        get { box.value }
+        set { box.value = newValue }
+    }
+    
+    private var box : Box
+    
+    fileprivate class Box {
+        
+        init(value: Value) {
+            self.value = value
+        }
+        
+        var value : Value
+    }
+}
+
+extension CopyOnWrite.Box : Equatable where Value:Equatable {
+    
+    static func == (lhs:CopyOnWrite.Box, rhs:CopyOnWrite.Box) -> Bool {
+        lhs === rhs || lhs.value == rhs.value
+    }
+    
+}
+
+extension CopyOnWrite : Equatable where Value:Equatable {}
